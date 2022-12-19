c42d4cb8749662d93f2b632ac686ed2ec9fbe53a
revenkogrisha <grisha@revenko.org>
2022-12-20 00:16:49 +0300

    Added subscribtion on Checker's event

--- Assets/_Scripts/Score.cs
+++ Assets/_Scripts/Score.cs
@@ -3,20 +3,33 @@ using UnityEngine;
 public class Score : MonoBehaviour
 {
     [SerializeField] private int _reward = 10;
+    [SerializeField] private CardCoincidenceChecker _checker;
     [SerializeField] private ScoreView _scoreView;
 
     private int Amount = 0;
+
+    #region MonoBehaviour
 
     private void Awake()
     {
         _scoreView.UpdateText(Amount);
     }
 
+    private void OnEnable()
+    {
+        _checker.OnMatch += Match;
+    }
+    
+    private void OnDisable()
+    {
+        _checker.OnMatch -= Match;
+    }
+
+    #endregion
+
     public void Match()
     {
         Amount += _reward;
         _scoreView.UpdateText(Amount);
     }
-
-    private void UpdateText() => _scoreView.UpdateText(Amount);
 }
